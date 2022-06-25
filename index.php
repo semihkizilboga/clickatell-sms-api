8238e0e9df22bf4ca385a6a55e81423cdeaf9df9
Semih Kızılboğa <78806541+semihkizilboga@users.noreply.github.com>
2022-06-25 04:45:25 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
   // TOPLU GÖNDERME
   
         $api = "API_KEY";
-	$no = ["05353512525","05353512525","05353512525"];
+
+	$numbers = ["05353512525","05353512525","05353512525"];
 	$message = urlencode("Deneme Mesaj");
 
-	foreach ($no as $item) {
+	foreach ($numbers as $item) {
 		file_get_contents("https://platform.clickatell.com/messages/http/send?apiKey=" . $api . "&to=" . $item . "&content=" . $message);
 	}
 
   
   // TEKLİ GÖNDERME
 
-  
-        $api = "API_KEY";
+ 
 	$no = "05353512525";
 	$message = urlencode("Deneme Mesaj");
  
