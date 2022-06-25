0e960014a9437ba4d87dcd051ac4868d5450735b
Semih Kızılboğa <78806541+semihkizilboga@users.noreply.github.com>
2022-06-25 04:46:43 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,25 +1,16 @@
 <?php
 
+  // ALL SEND
+  $api = "API_KEY";
+  $numbers = ["05353512525","05353512525","05353512525"];
+  $message = urlencode("Deneme Mesaj");
 
-  // TOPLU GÖNDERME
-  
-        $api = "API_KEY";
+  foreach ($numbers as $item) {
+    file_get_contents("https://platform.clickatell.com/messages/http/send?apiKey=" . $api . "&to=" . $item . "&content=" . $message);
+  }
 
-	$numbers = ["05353512525","05353512525","05353512525"];
-	$message = urlencode("Deneme Mesaj");
-
-	foreach ($numbers as $item) {
-		file_get_contents("https://platform.clickatell.com/messages/http/send?apiKey=" . $api . "&to=" . $item . "&content=" . $message);
-	}
-
-  
-  // TEKLİ GÖNDERME
-
- 
-	$no = "05353512525";
-	$message = urlencode("Deneme Mesaj");
- 
-	file_get_contents("https://platform.clickatell.com/messages/http/send?apiKey=" . $api . "&to=" . $item . "&content=" . $message); 
-
+  // SINGLE SEND
+  $no = "05353512525";
+  file_get_contents("https://platform.clickatell.com/messages/http/send?apiKey=" . $api . "&to=" . $item . "&content=" . $message); 
 
 ?>
